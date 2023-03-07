bbb78780459df6a02b67fcf4e18f808623f69abf
theeman05 <theeman05@gmail.com>
2023-03-06 20:37:30 -0500

    Created some files to get us started for random data creation.

---
+++ src/Modules/ID.py
@@ -0,0 +1,36 @@
+from GlobalStuff import increment_str, INCREMENT_START_CHAR
+
+DEFAULT_ID_LENGTH = 8  # Default Length of the id
+
+
+class ID:
+    """
+    Generic class for an ID. I figured this would be good because we will have multiple classes with ID's
+    """
+    def __init__(self, last_value: str | None = None, value: str = None, length: int = DEFAULT_ID_LENGTH):
+        """
+        Create an ID. The value will be extended to the length
+        :param last_value: incremented the last value to create a new ID. None returns the base id of the
+        given length (If a value is not given)
+        :param value: create an ID with the given value
+        :param length: length of the ID or the default ID length
+        :exception thrown if the value exceeds the length
+        """
+        if not value:
+            value = increment_str(last_value) if last_value else INCREMENT_START_CHAR * length
+
+        diff = len(value) - length
+        if diff < 0:  # Extend the ID to match the desired length
+            value = INCREMENT_START_CHAR * abs(diff) + value
+        elif diff != 0:  # Uh oh
+            raise Exception(f"The value {value} exceeds the length by {diff}.")
+
+        self.value = value
+
+    def __str__(self):
+        return f"ID: {self.value}"
+
+
+if __name__ == '__main__':
+    last = ID(value="12345")
+    print(last.value)
