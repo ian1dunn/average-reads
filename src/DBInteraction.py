d7dd4f1306582ec69d7c409ddc34c4e3ddc4c992
theeman05 <theeman05@gmail.com>
2023-03-22 13:31:07 -0400

    The Connection class has been updated to create an ongoing server connection until ConnectionClose is called.

--- src/DBInteraction.py
+++ src/DBInteraction.py
@@ -1,4 +1,3 @@
-
 import psycopg2
 from sshtunnel import SSHTunnelForwarder
 
@@ -6,75 +5,43 @@ from sshtunnel import SSHTunnelForwarder
 class Connection:
     def __init__(self):
         with open("DataCredentials.txt", "r") as file:
-            self.__username = file.readline()[:-1]
-            self.__password = file.readline()[:-1]
-            self.__dbName = file.readline()
-
-        
-
-    def Query(self, Query: str):
+            username = file.readline().strip()
+            password = file.readline().strip()
+            dbName = file.readline().strip()
 
         try:
-            with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
-                                    ssh_username=self.__username,
-                                    ssh_password=self.__password,
-                                    remote_bind_address=('localhost', 5432)) as server:
-                server.start()
-                print("SSH tunnel established")
-                params = {
-                    'database': self.__dbName,
-                    'user': self.__username,
-                    'password': self.__password,
-                    'host': 'localhost',
-                    'port': server.local_bind_port
-                }
+            self.server = server = SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                                             ssh_username=username,
+                                             ssh_password=password,
+                                             remote_bind_address=('localhost', 5432))
+            server.start()
+            print("SSH tunnel established")
+            params = {
+                'database': dbName,
+                'user': username,
+                'password': password,
+                'host': 'localhost',
+                'port': server.local_bind_port
+            }
 
-
-                conn = psycopg2.connect(**params)
-                print("Database connection established")
-                curr = conn.cursor()
-                curr.execute(Query)
-                rows = curr.fetchall()
-                curr.close()
-                conn.close()
-                return rows
+            self.connection = psycopg2.connect(**params)
+            print("Database connection established")
         except:
-            print("Connection failed")   
+            print("Connection failed")
 
-# class Connection:
-#     def __init__(self):
-#         with open("DataCredentials.txt", "r") as file:
-#             username = file.readline()[:-1]
-#             password = file.readline()[:-1]
-#             dbName = file.readline()
-#         try:
-#             with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
-#                                     ssh_username=username,
-#                                     ssh_password=password,
-#                                     remote_bind_address=('localhost', 5432)) as server:
-#                 server.start()
-#                 print("SSH tunnel established")
-#                 params = {
-#                     'database': dbName,
-#                     'user': username,
-#                     'password': password,
-#                     'host': 'localhost',
-#                     'port': server.local_bind_port
-#                 }
-#                 self.conn = psycopg2.connect(**params)
-#                 print("Database connection established")
-                
-#         except:
-#             print("Connection failed")   
-            
+    def Query(self, query: str) -> list[tuple] | None:
+        cur = self.connection.cursor()
+        cur.execute(query)
+        results = None
+        if query.startswith("SELECT"):
+            # Select is the only query which returns data (I think)
+            results = cur.fetchall()
+        else:
+            # Made a change to the database, let's commit our changes
+            self.connection.commit()
+        cur.close()
+        return results
 
-#     def Query(self, Query: str):
-#         curr = self.conn.cursor()
-#         curr.execute(Query)
-#         rows = curr.fetchall
-#         curr.close
-#         return rows
-
-#     def ConnClose(self):
-#         self.conn.close
-
+    def ConnectionClose(self):
+        self.server.close()
+        self.connection.close()
