1918dcb534c546219c160ba8d42d3a7f5a009e46
eeb2988 <eeb2988@g.rit.edu>
2023-04-13 11:54:07 -0400

    Setup Datapreprocessing Libraries

---
+++ dataprocess.py
@@ -0,0 +1,24 @@
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as mp
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+#Data Preprocessing checking age of reader and number of friends
+
+# TODO Query for grabbing each users age and # of friends
+
+# TODO Create training set 
+
+
+
+# sklearn models automatically standardize data values
+
+# No need for one hot encoding
+
+# Split train and test
+
+# Select desired model
+
